e28ecefaa2509aaa045844427dcc9202aeaee756
gapillan <gapillan@my.centennialcollege.ca>
2016-07-08 15:32:33 -0400

    Added a Class called TerrestrialPlanet with its fields and methods

---
+++ COMP123_S2016_Assignment3/TerrestrialPlanet.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_S2016_Assignment3
+{
+    public class TerrestrialPlanet
+    {
+        private bool _oxygen;
+
+        public bool Habitable()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool HasMoons()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
