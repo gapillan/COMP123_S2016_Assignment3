c67d51e1fd93b9f548f42e1d02c75ff9c3c51d08
gapillan <gapillan@my.centennialcollege.ca>
2016-07-08 15:26:36 -0400

    Added an abstract class called Planet

---
+++ COMP123_S2016_Assignment3/Planet.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace COMP123_S2016_Assignment3
+{
+    public abstract class Planet
+    {
+        private double _diameter;
+        private double _mass;
+        private int _moonCount;
+        private string _name;
+        private double _orbitalPeriod;
+        private int _ringCount;
+        private double _rotationPeriod;
+    }
+}
